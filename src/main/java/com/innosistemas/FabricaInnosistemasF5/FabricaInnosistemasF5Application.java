d24a63a95b0acc794948895f22be443a0edc3b28
JuanmaLop <juan.ocampo4@udea.edu.co>
2025-09-13 16:20:45 -0500

    Proyecto base

---
+++ src/main/java/com/innosistemas/FabricaInnosistemasF5/FabricaInnosistemasF5Application.java
@@ -0,0 +1,13 @@
+package com.innosistemas.FabricaInnosistemasF5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaInnosistemasF5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaInnosistemasF5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/innosistemas/FabricaInnosistemasF5/FabricaInnosistemasF5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innosistemas.FabricaInnosistemasF5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricaInnosistemasF5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
